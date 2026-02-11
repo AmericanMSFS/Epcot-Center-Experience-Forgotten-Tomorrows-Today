d491415075b0f99bbf59ad142440c286c7c7a641
AmericanMSFS <128946499+AmericanMSFS@users.noreply.github.com>
2026-02-11 00:13:32 +0000

    Refactor geolocation handling to improve code structure and readability

--- main.js
+++ main.js
@@ -6,6 +6,11 @@ function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.watchPosition(success, error);
     } else {
-        
+        text.innerHTML = "Geolocation is not supported by this browser.";
     }
+}
+
+function success(position) {
+  text.innerHTML = "Latitude: " + position.coords.latitude +
+  "<br>Longitude: " + position.coords.longitude;
 }
