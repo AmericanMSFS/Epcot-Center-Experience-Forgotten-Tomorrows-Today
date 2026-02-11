0e22e2fe919903738dd4e0d2cf2a138d3b8b09ff
AmericanMSFS <128946499+AmericanMSFS@users.noreply.github.com>
2026-02-11 00:01:25 +0000

    Basic javascript programming aswel as ui refinements.

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-print("Hello World")
+var location = null;
+const launchButton = document.getElementById('button');
+const text = document.getElementById('text');
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.watchPosition(success, error);
+    } else {
+        
+    }
+}
